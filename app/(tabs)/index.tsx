82a1b5444713dae7b45f135b8bffe52b5f9eeb14
Rudra Dey Sarkar <97943164+Rudra-Dey-Sarkar@users.noreply.github.com>
2024-08-07 22:34:14 +0530

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { View, Text, TouchableOpacity, Button, Image, StyleSheet } from 'react-native';
 import React, { useEffect, useState } from 'react';
-
+import styles from './styles';
 //The main App component that fetches and displays random user data.
 const App = () => {
     const [Users, setUsers] = useState([]); // This hook is use for storing all users data
@@ -48,7 +48,7 @@ const App = () => {
                         <TouchableOpacity style={styles.nextButton} onPress={() => { if (Index < Users.length) { setIndex(Index + 1) } }}><Text style={styles.nextText}>Next</Text></TouchableOpacity>
                     </View>
                 </>
-            ): (
+            ) : (
                 <View style={styles.loading}>
                     <Text style={styles.loading_text}>Loading.....</Text>
                 </View>
@@ -56,93 +56,5 @@ const App = () => {
         </View>
     );
 };
-
-//Styles for the App component and the user profile
-const styles = StyleSheet.create({
-    display: {
-        height: '100%',
-    },
-    user: {
-        margin: "auto",
-        padding: 5,
-    },
-    userInside: {
-        backgroundColor: "#C9C9C9",
-        padding: 10,
-        borderRadius: 10,
-        shadowColor: "#000",
-        shadowOffset: { width: 5, height: 15 },
-        shadowOpacity: 0.3,
-        shadowRadius: 5,
-    },
-    userPicture: {
-        margin: "auto",
-        width: 100,
-        height: 100,
-        borderWidth: 2,
-        borderColor: '#3b3b3b',
-        borderRadius: 50,
-    },
-    mainText: {
-        fontSize: 25,
-        margin: "auto",
-        fontWeight: "bold",
-    },
-    allText: {
-        fontSize: 15,
-        fontWeight: "600",
-        marginTop: 2,
-        marginBottom: 2,
-        padding: 5,
-        borderWidth: 2,
-        borderColor: '#3b3b3b',
-        borderRadius: 9,
-    },
-    buttonBar: {
-        height: "auto",
-        width: "100%",
-        padding: 10,
-        backgroundColor: "darkgrey",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    prevButton: {
-        margin: 5,
-        backgroundColor: "white",
-        padding: 10,
-        borderRadius: 25,
-        width: 100,
-    },
-    nextButton: {
-        margin: 5,
-        backgroundColor: "#3b3b3b",
-        padding: 10,
-        borderRadius: 25,
-        width: 100,
-    },
-    prevText: {
-        fontSize: 15,
-        fontWeight: "bold",
-        margin: "auto",
-    },
-    nextText: {
-        color: "white",
-        fontSize: 15,
-        fontWeight: "bold",
-        margin: "auto",
-    },
-    loading:{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    loading_text:{
-        fontSize: 20,
-        fontWeight: "600",
-    }
-})
 
 export default App;
